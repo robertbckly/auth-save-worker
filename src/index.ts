6d2236ebc457765feac0f6d14151e4fd3d5b74c3
Rob Buckley <robertbckly@gmail.com>
2024-12-28 22:29:01 +0000

    add generic code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+const REQUIRED_METHOD = 'POST';
+const REQUIRED_PROTOCOL = 'https';
+const REQUIRED_LOCAL_HOSTNAME = 'localhost';
+
 export default {
   async fetch(request) {
+    // Reject methods other than POST
+    if (request.method !== REQUIRED_METHOD) {
+      return new Response('Method not allowed', { status: 405 });
+    }
+
+    // Reject HTTP unless running locally
+    const url = new URL(request.url);
+    if (url.protocol !== REQUIRED_PROTOCOL && url.hostname !== REQUIRED_LOCAL_HOSTNAME) {
+      return new Response('HTTPS required', { status: 403 });
+    }
+
+    // Success
     return new Response(`Hello :-) ${request.url}`, { status: 200 });
   },
 } satisfies ExportedHandler<Env>;
