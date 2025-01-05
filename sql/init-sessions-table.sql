ea52146827e9f401ea809cd8bde04787dc238790
Rob Buckley <robertbckly@gmail.com>
2025-01-05 22:14:54 +0000

    validate session id length at db level

--- sql/init-sessions-table.sql
+++ sql/init-sessions-table.sql
@@ -1,2 +1,6 @@
 DROP TABLE IF EXISTS UserSessions;
-CREATE TABLE IF NOT EXISTS UserSessions (SessionID TEXT PRIMARY KEY, UserID TEXT);
+CREATE TABLE IF NOT EXISTS UserSessions (
+  SessionID TEXT PRIMARY KEY, 
+  UserID TEXT,
+  CHECK (LENGTH(SessionID) >= 32) -- 128-bit minimum
+);
